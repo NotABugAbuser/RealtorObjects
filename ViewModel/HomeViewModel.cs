c7827a385db8445db927b851138ec1c01c3b34d2
Eugene <jenyagvozd@mail.ru>
2021-02-12 18:12:02 +0300

    Create filter area section layout

--- ViewModel/HomeViewModel.cs
+++ ViewModel/HomeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using RealtyModel.Model.Base;
+using RealtyModel.Service;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -43,12 +44,30 @@ namespace RealtorObjects.ViewModel
             false,
             false
         };
-
+        private ObservableCollection<CheckAndHeightPair> filterAreaSections = new ObservableCollection<CheckAndHeightPair>() { 
+            new CheckAndHeightPair(true, 200),
+            new CheckAndHeightPair(false, 50),
+            new CheckAndHeightPair(false, 50)
+        };
         private ObservableCollection<BaseRealtorObject> originalObjectList = new ObservableCollection<BaseRealtorObject>() {
         };
+        private CustomCommand openOrCloseFilterSection;
+        public CustomCommand OpenOrCloseFilterSection => openOrCloseFilterSection ?? (openOrCloseFilterSection = new CustomCommand(obj => {
+            object[] objects = obj as object[];
+            byte index = Convert.ToByte(objects[0]);
+            Int16 height = Convert.ToInt16(objects[1]);
+
+            if (!FilterAreaSections[index].Key) {
+                FilterAreaSections[index].Value = 50;
+            } else {
+                FilterAreaSections[index].Value = height;
+            }
+        }));
         public ObservableCollection<bool> ToggledButtons {
-            get => toggledButtons; set => toggledButtons = value;
+            get => toggledButtons; 
+            set => toggledButtons = value;
         }
+        public ObservableCollection<CheckAndHeightPair> FilterAreaSections => filterAreaSections;
         public ObservableCollection<BaseRealtorObject> OriginalObjectList => originalObjectList;
     }
 }
