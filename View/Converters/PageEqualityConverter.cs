d61abfc37f42af6e59bf36260144f831dd040331
Евгений Гвоздиков <jenyagvozd@mail.ru>
2021-04-23 13:05:06 +0300

    Merge the master commit with the page creating commit

---
+++ View/Converters/PageEqualityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RealtorObjects.View.Converters
+{
+    class PageEqualityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            if (values[0] == DependencyProperty.UnsetValue || values[1] == DependencyProperty.UnsetValue) {
+                return false;
+            } else {
+                return System.Convert.ToInt16(values[0]) == System.Convert.ToInt16(values[1]);
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ViewModel/HomeViewModel.cs
+++ ViewModel/HomeViewModel.cs
@@ -26,109 +26,89 @@ namespace RealtorObjects.ViewModel
     public class HomeViewModel : BaseViewModel
     {
         public event OpeningFlatFormEventHandler OpeningFlatForm;
-        public CustomCommand OpenCloseFilters => openCloseFilters ?? (openCloseFilters = new CustomCommand(obj =>
-        {
-            if (WidthOfFilters == 200)
-            {
+        public CustomCommand OpenCloseFilters => openCloseFilters ?? (openCloseFilters = new CustomCommand(obj => {
+            if (WidthOfFilters == 200) {
                 WidthOfFilters = 0;
-            }
-            else
-            {
+            } else {
                 WidthOfFilters = 200;
             }
         }));
-        public CustomCommand CreateRealtorObject => createRealtorObject ?? (createRealtorObject = new CustomCommand(obj =>
-        {
+        public CustomCommand CreateRealtorObject => createRealtorObject ?? (createRealtorObject = new CustomCommand(obj => {
             string type = (string)obj;
             if (type == "Flat")
-                    OpeningFlatForm?.Invoke(this, new OpeningFlatFormEventArgs(true, new Flat()));
+                OpeningFlatForm?.Invoke(this, new OpeningFlatFormEventArgs(true, new Flat()));
             //if (type == "House") ;
         }));
-        public CustomCommand Modify => modify ?? (modify = new CustomCommand(obj =>
-        {
+        public CustomCommand Modify => modify ?? (modify = new CustomCommand(obj => {
             BaseRealtorObject bro = (BaseRealtorObject)obj;
-            if (CheckAccess(bro.Agent, ((App)Application.Current).Credential.Name))
-            {
-                if (bro is Flat flat) 
+            if (CheckAccess(bro.Agent, ((App)Application.Current).Credential.Name)) {
+                if (bro is Flat flat)
                     OpeningFlatForm?.Invoke(this, new OpeningFlatFormEventArgs(false, flat));
                 //    if (bro is House house) RealtorObjectOperator.ModifyHouse(house);
             }
         }));
-        public CustomCommand Delete => delete ?? (delete = new CustomCommand(obj =>
-        {
+        public CustomCommand Delete => delete ?? (delete = new CustomCommand(obj => {
             BaseRealtorObject bro = (BaseRealtorObject)obj;
-            if (CheckAccess(bro.Agent, ((App)Application.Current).Credential.Name))
-            {
+            if (CheckAccess(bro.Agent, ((App)Application.Current).Credential.Name)) {
                 AllObjects.RemoveAll(x => x.Id == bro.Id);
                 CurrentObjectList.Remove(bro);
                 Operation operation = new Operation(((App)Application.Current).Credential.Name, bro.Id.ToString(), OperationDirection.Realty, OperationType.Remove);
             }
         }));
-        public CustomCommand TestCommand => testCommand ?? (testCommand = new CustomCommand(obj =>
-        {
+        public CustomCommand TestCommand => testCommand ?? (testCommand = new CustomCommand(obj => {
             FlatGenerator flatGenerator = new FlatGenerator();
-            foreach (BaseRealtorObject bro in flatGenerator.CreateFlatList(AllObjects.Count, AllObjects.Count + 50))
-            {
+            foreach (BaseRealtorObject bro in flatGenerator.CreateFlatList(AllObjects.Count, AllObjects.Count + 50)) {
                 bro.Agent = "ГвоздиковЕА";
                 AllObjects.Add(bro);
             }
         }));
-        public CustomCommand FilterCollection => filterCollection ?? (filterCollection = new CustomCommand(obj =>
-        {
+        public CustomCommand FilterCollection => filterCollection ?? (filterCollection = new CustomCommand(obj => {
             List<BaseRealtorObject> filteredObjects = Filter.CreateFilteredList(AllObjects);
+            Debug.WriteLine($"Фильтрованная: {filteredObjects.Count}");
             SplitFilteredCollection(filteredObjects, 25);
+            Pages.Clear();
+            CalculatePages(1);
+            CurrentPage = 1;
+            Debug.WriteLine($"Фильтрованная {filteredObjects.Count} и листов {ObjectLists.Count}");
             GC.Collect();
         }));
-        public CustomCommand OpenOrCloseFilterSection => openOrCloseFilterSection ?? (openOrCloseFilterSection = new CustomCommand(obj =>
-        {
+        public CustomCommand OpenOrCloseFilterSection => openOrCloseFilterSection ?? (openOrCloseFilterSection = new CustomCommand(obj => {
             object[] objects = obj as object[];
             byte index = Convert.ToByte(objects[0]);
             Int16 height = Convert.ToInt16(objects[1]);
 
-            if (!FilterAreaSections[index].Check)
-            {
+            if (!FilterAreaSections[index].Check) {
                 FilterAreaSections[index].Height = 50;
-            }
-            else
-            {
+            } else {
                 FilterAreaSections[index].Height = height;
             }
         }));
-        private bool CheckAccess(string objectAgent, string currentAgent)
-        {
-            if (objectAgent == currentAgent)
-            {
+        private bool CheckAccess(string objectAgent, string currentAgent) {
+            if (objectAgent == currentAgent) {
                 return true;
-            }
-            else
-            {
+            } else {
                 MessageBox.Show("У вас нет права на доступ к этому объекту");
                 return false;
             }
         }
-        private void SplitFilteredCollection(List<BaseRealtorObject> filteredObjects, byte count)
-        {
+        private void SplitFilteredCollection(List<BaseRealtorObject> filteredObjects, byte count) {
             //разбиение фильтрованной коллекции на parts частей по count объектов
             Int16 parts = (Int16)Math.Ceiling(filteredObjects.Count / (double)count);
             List<List<BaseRealtorObject>> lists = Enumerable.Range(0, parts).AsParallel().Select(x => filteredObjects.Skip(x * count).Take(count).ToList()).ToList();
             ObjectLists.Clear();
             CurrentObjectList.Clear();
-            foreach (List<BaseRealtorObject> ol in lists)
-            {
+            foreach (List<BaseRealtorObject> ol in lists) {
                 ObjectLists.Add(new ObservableCollection<BaseRealtorObject>(ol));
             }
-            if (ObjectLists.Count != 0)
-            {
+            if (ObjectLists.Count != 0) {
                 CurrentObjectList = ObjectLists[0];
             }
         }
 
-        public void OnAddHouse()
-        {
+        public void OnAddHouse() {
 
         }
-        private void TestMethod()
-        {
+        private void TestMethod() {
             FlatGenerator flatGenerator = new FlatGenerator();
             Flat flat = flatGenerator.CreateFlat();
             flat.Id = 9999;
@@ -137,10 +117,8 @@ namespace RealtorObjects.ViewModel
             CurrentObjectList.Add(flat);
             AllObjects.Add(flat);
         }
-        public void ReceiveUpdate(ReceivedObjectDBEventArgs e)
-        {
-            if(e.TargetType == TargetType.Flat)
-            {
+        public void ReceiveUpdate(ReceivedObjectDBEventArgs e) {
+            if (e.TargetType == TargetType.Flat) {
                 Flat[] flats = JsonSerializer.Deserialize<Flat[]>((String)e.UpdateData);
                 allObjects = new List<BaseRealtorObject>(flats);
             }
@@ -154,6 +132,7 @@ namespace RealtorObjects.ViewModel
         private CustomCommand filterCollection;
         private CustomCommand delete;
         private CustomCommand modify;
+        private CustomCommand goToPage;
         private CustomCommand createRealtorObject;
         private CustomCommand openCloseFilters;
         private Filter filter = new Filter();
@@ -170,44 +149,91 @@ namespace RealtorObjects.ViewModel
             new CheckAndHeightPair(false, 50),
         };
         private double widthOfFilters = 200;
+        private int currentPage = 1;
+        private ObservableCollection<int> pages = new ObservableCollection<int>() {
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            123
+        };
+        public CustomCommand GoToPage => goToPage ?? (goToPage = new CustomCommand(obj => {
+            Int16 index = (Int16)(Convert.ToInt16(obj) - 1);
+            Pages.Clear();
+            CalculatePages(index);
+            CurrentPage = index + 1;
+            CurrentObjectList = ObjectLists[index];
+        }));
 
-        public HomeViewModel()
-        {
-            //RealtorObjectOperator.Client = this.Client;
+
+        private void CalculatePages(short currentPage) {
+            int count = ObjectLists.Count;
+            if (count < 15) {
+                for (int i = 0; i < count; i++) { Pages.Add(i + 1); }
+            } else {
+                int left = 7;
+                int right = 7;
+                if (currentPage + 8 > count) {
+                    int difference = -(count - 8 - currentPage);
+                    left += difference;
+                    right -= difference;
+                }
+                if (currentPage - 7 < 0) {
+                    int difference = -(currentPage - 7);
+                    left -= difference;
+                    right += difference;
+                }
+                for (int i = left; i > 0; i--) { Pages.Add(currentPage - i + 1); }
+                for (int i = 0; i < right + 1; i++) { Pages.Add(currentPage + i + 1); }
+            }
+        }
+        public HomeViewModel() {
             TestMethod();
         }
-
-        public ObservableCollection<BaseRealtorObject> CurrentObjectList
-        {
+        public ObservableCollection<int> Pages {
+            get => pages; set => pages = value;
+        }
+        public int CurrentPage {
+            get => currentPage;
+            set {
+                currentPage = value;
+                OnPropertyChanged();
+            }
+        }
+        public ObservableCollection<BaseRealtorObject> CurrentObjectList {
             get => currentObjectList;
-            set
-            {
+            set {
                 currentObjectList = value;
                 OnPropertyChanged();
             }
         }
-        public List<ObservableCollection<BaseRealtorObject>> ObjectLists
-        {
+        public List<ObservableCollection<BaseRealtorObject>> ObjectLists {
             get => objectLists;
             set => objectLists = value;
         }
-        public List<BaseRealtorObject> AllObjects
-        {
+        public List<BaseRealtorObject> AllObjects {
             get => allObjects;
             set => allObjects = value;
         }
         public ObservableCollection<CheckAndHeightPair> FilterAreaSections => filterAreaSections;
-        public Filter Filter
-        {
+        public Filter Filter {
             get => filter;
             set => filter = value;
         }
 
-        public double WidthOfFilters
-        {
+        public double WidthOfFilters {
             get => widthOfFilters;
-            set
-            {
+            set {
                 widthOfFilters = value;
                 OnPropertyChanged();
             }
